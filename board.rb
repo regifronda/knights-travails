163d9f3eac7cba46e964cfa3168a354ff3dba177
Regi Fronda <regisfronda@gmail.com>
2021-07-29 02:07:55 -0700

    Add render method

--- board.rb
+++ board.rb
@@ -1,11 +1,17 @@
 class Board
-  #initialize
+  attr_accessor :board
   def initialize
-    #set up blank data structure
     @board = Array.new(8) { Array.new(8) }
   end
 
-  # render method
-    # loop through data structure
-      # display knight. Else, blank
+  def render
+    puts
+    board.each do |row|
+      row.each do |cell|
+      cell.nil? ? print("-") : print(cell.to_s)
+      end
+      puts
+    end
+    puts
+  end
 end
