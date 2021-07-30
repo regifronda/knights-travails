2f91b02bf1bfd2f5cf509515f1423ee99af10762
Regi Fronda <regisfronda@gmail.com>
2021-07-29 18:35:05 -0700

    Add add_piece method

--- board.rb
+++ board.rb
@@ -1,5 +1,6 @@
 class Board
   attr_accessor :board
+  
   def initialize
     @board = Array.new(8) { Array.new(8) }
   end
@@ -13,5 +14,9 @@ class Board
       puts
     end
     puts
+  end
+
+  def add_piece(starting_square, piece)
+    board[starting_square[0]][starting_square[1]] = piece
   end
 end
