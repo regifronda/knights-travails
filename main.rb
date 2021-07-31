ca07ff72044bf2aae6c13da835c87859de929677
Regi Fronda <regisfronda@gmail.com>
2021-07-31 05:05:25 -0700

    Add black knight symbol for knight piece

--- main.rb
+++ main.rb
@@ -3,12 +3,18 @@ require_relative 'knight'
 
 class KnightsTravails
   attr_accessor :board, :knight
+  
   def initialize
     @board = Board.new
-    board.render
+    @board.render
     # set up knight
-    @knight = Knight.new
+    @knight = Knight.new("♞", board)
+  end
+  
+  def test_placement
+    @knight.knight_moves([0, 1], [2, 0])
   end
 end
 
 knights_travails = KnightsTravails.new
+knights_travails.test_placement
