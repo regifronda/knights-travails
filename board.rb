9ae1d60b0bebee4d90503c512ce295a8f9063a47
Regi Fronda <regisfronda@gmail.com>
2021-07-31 23:00:07 -0700

    Move pseudocode from knight.rb to board.rb

--- board.rb
+++ board.rb
@@ -20,4 +20,28 @@ class Board
     @board = Array.new(8) { Array.new(8) }
     @board[square[0]][square[1]] = piece
   end
+    # Treat all possible moves the knight could make as children in a tree
+    # Have adjacent nodes
+    # Build graph, connecting all possible moves
+    # Generate all possibilities from current square
+      # use two direction arrays for bfs, row and column
+      # All next legal moves include row + or - 2 and column + or - 1 as well as
+      # row + or - 1 and column + or - 2
+      column_movement = [2, 2, -2, 2, 1, 1, -1, -1]
+      row_movement = [1, -1, 1, -1, 2, -2, 2, -2]
+      # Remove moves that aren't on the board like -1 or 8
+  def inside_board?
+
+  end
+  # Use BFS to search for shortest path between starting square and ending square
+    # Initialize discovered_nodes_and distance array, which keeps track of the discovered nodes/squares and number of moves from startin square
+    # Initialize array that serves as queue 
+    # Insert starting_square/node
+    # loop while the queue is not empty
+      # Take the front node from the queue into variable current
+      # Get all possible squares and nodes from current and add into the queue and discovered arrays
+      # Don't mark already visited squares or moves that go off the board as discovered nodes 
+        #because that will prevent the queue from ever being empty
+      # As soon as you discover the ending square, you find the minimum number of moves
+        # Return after discovering ending square
 end
