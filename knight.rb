c310ea9c6f711cf9043304cceaf8826bed824623
Regi Fronda <regisfronda@gmail.com>
2021-08-01 01:40:08 -0700

    Add @jump_movements instance variable

--- knight.rb
+++ knight.rb
@@ -1,5 +1,5 @@
 class Knight
-  attr_accessor :piece_symbol, :board, :column, :row, :distance
+  attr_accessor :piece_symbol, :board, :column, :row, :distance, :jump_movements
 
   # Give attributes like coordinates and adjacent_nodes to help build a tree
   def initialize(piece_symbol, board)
@@ -8,6 +8,14 @@ class Knight
     @column = column
     @row = row
     @distance = distance
+    # Treat all possible moves the knight could make as children in a tree
+    # Have adjacent nodes
+    # Build graph, connecting all possible moves
+    # Generate all possibilities from current square
+      # use two direction arrays for bfs, row and column
+      # All next legal moves include row + or - 2 and column + or - 1 as well as
+      # row + or - 1 and column + or - 2
+    @jump_movements = [[-2, -1], [-2, 1], [-1, -2], [-1, 2], [1, -2], [1, 2], [2, -1], [2, 1]]
   end
 
   def knight_moves(starting_square, ending_square)
@@ -16,23 +24,13 @@ class Knight
     @board.add_piece(ending_square, piece_symbol)
     @board.render
   end
-  # Treat all possible moves the knight could make as children in a tree
-    # Have adjacent nodes
-    # Build graph, connecting all possible moves
-    # Generate all possibilities from current square
-      # use two direction arrays for bfs, row and column
-      # All next legal moves include row + or - 2 and column + or - 1 as well as
-      # row + or - 1 and column + or - 2
-  column_movement = [2, 2, -2, 2, 1, 1, -1, -1]
-  row_movement = [1, -1, 1, -1, 2, -2, 2, -2]
-      # Remove moves that aren't on the board like -1 or 8
+      
+  # Remove moves that aren't on the board like -1 or 8
   def legal_move?(column_movement, row_movement)
-    if (column_movement >=1 && column_movement <= 8) &&
-      (row_movement >= 1 && row_movement <= 8)
-      puts "true"
+    if (column_movement >= 0 && column_movement <= 7) &&
+      (row_movement >= 0 && row_movement <= 7)
       return true
     else 
-      puts "false"
       return false
     end
   end
